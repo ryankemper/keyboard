e323e68f00d43a038bc33c10e1dbe8a4aa24964c
Ryan Kemper <ryankemper@umail.ucsb.edu>
2019-05-03 10:14:04 -0700

    Add our own hyper key shortcuts

---
+++ hammerspoon/hyper-apps.lua
@@ -0,0 +1,15 @@
+-- Default keybindings for launching apps in Hyper Mode
+--
+-- To launch _your_ most commonly-used apps via Hyper Mode, create a copy of
+-- this file, save it as `hyper-apps.lua`, and edit the table below to configure
+-- your preferred shortcuts.
+return {
+  { 'a', 'iTunes' },            -- "A" for "Apple Music"
+  { 'b', 'Google Chrome' },     -- "B" for "Browser"
+  { 'c', 'Slack' },             -- "C for "Chat"
+  { 'd', 'Remember The Milk' }, -- "D" for "Do!" ... or "Done!"
+  { 'e', 'Sublime Text' },      -- "E" for "Editor"
+  { 'f', 'Finder' },            -- "F" for "Finder"
+  { 's', 'Slack' },             -- "S" for "Slack"
+  { 't', 'Terminal' },          -- "T" for "Terminal"
+}
